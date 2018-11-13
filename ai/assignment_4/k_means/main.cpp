ffa72286c6f38ae3812985d71a36df25fb4969fa
PJ-Leyden <pleyden@kent.edu>
2018-11-13 14:21:30 -0500

    Added main for k_means

---
+++ ai/assignment_4/k_means/main.cpp
@@ -0,0 +1,23 @@
+#include "cluster.hpp"
+#include "points.hpp"
+
+#include <string>
+
+int main(){//hard or soft clustering? (hard hopefully)
+
+	//generate random points
+
+	//decide on number of clusters
+
+	//create clusters
+
+	//generate random centroid for clusters
+
+	//{
+		//determine membership
+
+		//re calc clusters
+	//} end when no point moves. 
+	
+
+}
